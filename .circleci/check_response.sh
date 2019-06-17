478214f4c480ebd33c27d06843ffe960a3cc864c
jacekelgda <jacekelgda@gmail.com>
2019-06-17 17:38:25 +0200

    Job for checking access token

---
+++ .circleci/check_response.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+API_RESPONSE=$(curl -s -o /dev/null -w "%{http_code}" $JOB_PIM_API_ROUTE --header "authorization: $JOB_PIM_ACCESS_TOKEN")
+NOT_AUTHORIZED="401"
+
+if [ "$API_RESPONSE" = "$NOT_AUTHORIZED" ]; then
+    NEW_TOKEN=$(curl --request POST \
+    --url $JOB_AUTH_DOMAIN \
+    --header 'content-type: application/json' \
+    --data "{\"client_id\":\"$JOB_AUTH_CLIENT_ID\",\"client_secret\":\"$JOB_AUTH_SECRET\",\"audience\":\"$JOB_AUTH_AUDIENCE\",\"grant_type\":\"client_credentials\"}")
+
+    echo "$NEW_TOKEN" > workspace/echo-output
+else
+  echo "Token is valid"
+fi
